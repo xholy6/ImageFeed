0491def7685c58a7bc17853730340bba4cad831d
xholy6 <blast6xgit@gmail.com>
2023-02-07 14:48:28 +0300

    added tableview with list of images

---
+++ Image Feed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var imageCell: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+}
